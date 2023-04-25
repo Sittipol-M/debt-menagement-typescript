4e4e974b47e4e1f15601a9de031f6f547fff9c02
Sittipol Manitkul <workpapio@gmail.com>
2023-04-25 11:29:45 +0700

    Add dependency injection group service

--- src/group/GroupService.ts
+++ src/group/GroupService.ts
@@ -3,21 +3,24 @@ import Group from "./Group";
 import GroupRepository from "./GroupRepository";
 import DuplicationError from "../others/error/DuplicationError";
 import NotFoundError from "../others/error/NotFoundError";
-
-const groupRepository: GroupRepository = new GroupRepository();
-
 class GroupService {
+  private readonly groupRepository: GroupRepository;
+
+  public constructor(groupRepository: GroupRepository) {
+    this.groupRepository = groupRepository;
+  }
+
   public addNewGroup = async (req: Request): Promise<Group> => {
     const { name, description } = req.body;
     const group = new Group(null, name, description);
-    const newGroup = await groupRepository.addNewGroup(group);
+    const newGroup = await this.groupRepository.addNewGroup(group);
     return newGroup;
   };
 
   public checkGroupIsCreated = async (name: string): Promise<void> => {
     const group: Group = new Group();
     group.name = name;
-    const isExisted = await groupRepository.isGroupExisted(group);
+    const isExisted = await this.groupRepository.isGroupExisted(group);
     if (isExisted) {
       throw new DuplicationError("Group's name is used");
     }
@@ -26,14 +29,14 @@ class GroupService {
   public checkGroupNotExistedById = async (id: number): Promise<void> => {
     const group: Group = new Group();
     group.id = id;
-    const isExisted = await groupRepository.isGroupExisted(group);
+    const isExisted = await this.groupRepository.isGroupExisted(group);
     if (!isExisted) {
       throw new NotFoundError("Group not found");
     }
   };
 
   public getGroups = async (): Promise<Array<Group>> => {
-    const groups = await groupRepository.findAll();
+    const groups = await this.groupRepository.findAll();
     return groups;
   };
 }
