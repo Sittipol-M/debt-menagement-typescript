c595f42f6ac9ee892ea3c249cc978d277a5d5062
Sittipol Manitkul <workpapio@gmail.com>
2023-04-25 11:32:33 +0700

    Add dependency injection group member controller

--- src/groupMember/GroupMemberController.ts
+++ src/groupMember/GroupMemberController.ts
@@ -5,20 +5,30 @@ import GroupMemberValidation from "./GroupMemberValidation";
 import GroupService from "../group/GroupService";
 import GroupMember from "./GroupMember";
 
-const groupMemberService = new GroupMemberService();
-const groupMemberValidation = new GroupMemberValidation();
-const groupService = new GroupService();
-
 class GroupMemberController {
+  private readonly groupMemberService: GroupMemberService;
+  private readonly groupMemberValidation: GroupMemberValidation;
+  private readonly groupService: GroupService;
+
+  public constructor(
+    groupMemberService: GroupMemberService,
+    groupMemberValidation: GroupMemberValidation,
+    groupService: GroupService
+  ) {
+    this.groupMemberService = groupMemberService;
+    this.groupMemberValidation = groupMemberValidation;
+    this.groupService = groupService;
+  }
+
   public addNewGroupMember = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { groupId } = req.params;
       const { name } = req.body;
-      await groupMemberValidation.validateGroupMembersRequestParams(req);
-      await groupMemberValidation.validateAddNewGroupMemberRequestBody(req);
-      await groupService.checkGroupNotExistedById(Number(groupId));
-      await groupMemberService.checkGroupMemberIsCreated(Number(groupId), name);
-      const newGroupMember: GroupMember = await groupMemberService.addNewGroupMember(req);
+      await this.groupMemberValidation.validateGroupMembersRequestParams(req);
+      await this.groupMemberValidation.validateAddNewGroupMemberRequestBody(req);
+      await this.groupService.checkGroupNotExistedById(Number(groupId));
+      await this.groupMemberService.checkGroupMemberIsCreated(Number(groupId), name);
+      const newGroupMember: GroupMember = await this.groupMemberService.addNewGroupMember(req);
       res.status(HttpStatus.CREATED).json({ message: "Add new group's member successful", newGroupMember });
     } catch (error) {
       next(error);
@@ -28,8 +38,8 @@ class GroupMemberController {
   public getGroupMembers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { groupId } = req.body;
-      await groupMemberValidation.validateGroupMembersRequestParams(req);
-      const groupMembers = await groupMemberService.getGroupMembersById(Number(groupId));
+      await this.groupMemberValidation.validateGroupMembersRequestParams(req);
+      const groupMembers = await this.groupMemberService.getGroupMembersById(Number(groupId));
       res.status(HttpStatus.OK).json({ message: "Get group's members successful", groupMembers });
     } catch (error) {
       next(error);
