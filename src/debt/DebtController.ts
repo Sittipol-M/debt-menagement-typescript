9df095bf9b8c30d7dc99136493dc50d511661d46
Sittipol Manitkul <workpapio@gmail.com>
2023-04-25 11:16:50 +0700

    Add dependency injection debt controller

--- src/debt/DebtController.ts
+++ src/debt/DebtController.ts
@@ -4,21 +4,26 @@ import DebtValidation from "./DebtValidation";
 import GroupService from "../group/GroupService";
 import DebtService from "./DebtService";
 import Debt from "./Debt";
-
-const debtValidation = new DebtValidation();
-const groupService = new GroupService();
-const debtService = new DebtService();
-
 class DebtController {
+  private readonly debtValidation: DebtValidation;
+  private readonly debtService: DebtService;
+  private readonly groupService: GroupService;
+
+  public constructor(debtValidation: DebtValidation, debtService: DebtService, groupService: GroupService) {
+    this.debtValidation = debtValidation;
+    this.debtService = debtService;
+    this.groupService = groupService;
+  }
+
   public addNewDebt = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { groupId } = req.params;
       const { name } = req.body;
-      await debtValidation.validateAddNewDebtRequestParams(req);
-      await debtValidation.validateAddNewDebtRequestBody(req);
-      await groupService.checkGroupNotExistedById(Number(groupId));
-      await debtService.checkDebtIsCreated(Number(groupId), name);
-      const newDebt: Debt = await debtService.addNewDebt(req);
+      await this.debtValidation.validateAddNewDebtRequestParams(req);
+      await this.debtValidation.validateAddNewDebtRequestBody(req);
+      await this.groupService.checkGroupNotExistedById(Number(groupId));
+      await this.debtService.checkDebtIsCreated(Number(groupId), name);
+      const newDebt: Debt = await this.debtService.addNewDebt(req);
       res.status(HttpStatus.CREATED).json({ message: "Add new debt successful", newDebt });
     } catch (error) {
       next(error);
@@ -28,8 +33,8 @@ class DebtController {
   public getDebts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { groupId } = req.params;
-      await debtValidation.validateAddNewDebtRequestParams(req);
-      const debts: Array<Debt> = await debtService.getDebts(Number(groupId));
+      await this.debtValidation.validateAddNewDebtRequestParams(req);
+      const debts: Array<Debt> = await this.debtService.getDebts(Number(groupId));
       res.status(HttpStatus.OK).json({ message: "Get debts successful", debts });
     } catch (error) {
       next(error);
